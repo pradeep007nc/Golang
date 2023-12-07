83b9118d98dd0c305e3f6ec5b9d6680b7ca4f962
pradeep007nc <pradeepnaidu2486@gmail.com>
2023-12-07 23:31:47 +0530

    updated problem

--- leetcode/array_and_hash/GroupAnagrams.go
+++ leetcode/array_and_hash/GroupAnagrams.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 )
 
-// error passes 117 test case out of 120
 func GroupAnagrams(strs *[]string) [][]string {
 	dict := make(map[string][]string, len(*strs))
 
@@ -14,14 +13,19 @@ func GroupAnagrams(strs *[]string) [][]string {
 		for _, ch := range val {
 			freq[ch-'a']++
 		}
+
+		// Convert the frequency array to a string to use as a key
 		key := ""
-		for _, count := range freq {
-			key += strconv.Itoa(count)
+		for i, count := range freq {
+			// Append the character and its count to the key
+			key += string('a'+i) + strconv.Itoa(count)
 		}
 
+		// Append the string to the corresponding group in the map
 		dict[key] = append(dict[key], val)
 	}
 
+	// Prepare the final result by converting the map values to a slice
 	ans := make([][]string, 0, len(dict))
 	for _, val := range dict {
 		ans = append(ans, val)
@@ -49,4 +53,27 @@ func GroupAnagrams2(strs []string) [][]string {
 	}
 
 	return ans
+}
+
+// optimised
+func GroupAnagram3(strs *[]string) [][]string {
+	freqs := make(map[[26]byte][]string, len(*strs))
+
+	for _, s := range *strs {
+		g := [26]byte{}
+
+		for _, c := range s {
+			g[c-'a']++
+		}
+
+		freqs[g] = append(freqs[g], s)
+	}
+
+	result := [][]string{}
+
+	for _, v := range freqs {
+		result = append(result, v)
+	}
+
+	return result
 }
