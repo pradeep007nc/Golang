6303c9995e644e56214e845017b28831f8adace5
pradeep007nc <pradeepnaidu2486@gmail.com>
2023-12-27 21:03:10 +0530

    updated models

--- adv/restaurant-management/models/noteModel.go
+++ adv/restaurant-management/models/noteModel.go
@@ -8,9 +8,9 @@ import (
 
 type Note struct {
 	Id         primitive.ObjectID `bson:"_id"`
-	Text       string             `bson:"text"`
-	Title      string             `bson:"title"`
-	Created_at time.Time          `bson:"created_at"`
-	Updated_at time.Time          `bson:"updated_at"`
-	Note_id    string             `bson:"note_id"`
+	Text       string             `json:"text"`
+	Title      string             `json:"title"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Note_id    string             `json:"note_id"`
 }
